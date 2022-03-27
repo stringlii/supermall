1b073aed76c658a3cd602672b3d7f23659f7c50c
李天一 <stringli@qq.com>
2022-03-27 14:15:55 +0800

    [add] 过滤敏感数据

--- supermall-product/src/main/java/com/litianyi/supermall/product/config/RedissonConfiguration.java
+++ supermall-product/src/main/java/com/litianyi/supermall/product/config/RedissonConfiguration.java
@@ -3,6 +3,7 @@ package com.litianyi.supermall.product.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,9 +21,11 @@ public class RedissonConfiguration {
      * 使用 RedissonClient 操作 redisson
      */
     @Bean(destroyMethod = "shutdown")
-    RedissonClient redisson() throws IOException {
+    RedissonClient redisson(@Value("${spring.redis.host}") String url,
+                            @Value("${spring.redis.port}") Integer port,
+                            @Value("${spring.redis.password}") String password) {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setPassword("123456");
+        config.useSingleServer().setAddress("redis://" + url + ":" + port).setPassword(password);
         return Redisson.create(config);
     }
 }
