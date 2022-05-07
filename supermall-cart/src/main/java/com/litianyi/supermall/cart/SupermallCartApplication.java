a0060acca495c2c7534c1bfa4355414e936ea71e
stringli <stringli@qq.com>
2022-05-07 15:41:00 +0800

    [add] 初始化购物车服务

---
+++ supermall-cart/src/main/java/com/litianyi/supermall/cart/SupermallCartApplication.java
@@ -0,0 +1,13 @@
+package com.litianyi.supermall.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermallCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermallCartApplication.class, args);
+    }
+
+}
---
+++ supermall-cart/src/test/java/com/litianyi/supermall/cart/SupermallCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litianyi.supermall.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermallCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
